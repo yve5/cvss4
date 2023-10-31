2acb1f93db878a073f5bd9a6c78f61a416694bbf
Yves Roll <yves.rouille@gmail.com>
2023-10-31 18:49:52 +0100

    container

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Getting Started with Create React App
+# CVSS v4 Calculator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Pitch
+
+This project proposes a CVSS v4.0 calculator with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Scripts
 
@@ -11,3 +13,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 | `npm run coverage` | Launches the test runner in order to generate a project full coverage report. |
 | `npm run build`    | Builds the app for production to the `build` folder.                          |
 | `npm run deploy`   | Deploy the `build` folder content in Github project page.                     |
+
+## Source
+
+[Original CVSS v4.0 calculator](https://github.com/RedHatProductSecurity/cvss-v4-calculator/tree/main).
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,98 @@
 import React from 'react';
-import logo from '../resources/logo.svg';
-import '../styles/App.css';
+import { cvssConfig } from '../resources/cvssConfig';
+// import logo from '../resources/logo.svg';
+// import '../styles/App.css';
+
+// console.log(Object.entries(cvssConfig));
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div className="App container">
+    <div className="row">
+      <div className="col">
+        {/*
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        */}
+
+        {Object.entries(cvssConfig).map(
+          ([metricType, { fill, metric_groups: metricGroups }]) => (
+            <div className="card my-3" key={metricType}>
+              <div className="card-header text-center">
+                {metricType}{' '}
+                <sup>
+                  <abbr
+                    title={`This category is usually filled by the ${fill}`}
+                  >
+                    ?
+                  </abbr>
+                </sup>
+              </div>
+              {Object.entries(metricGroups).map(
+                ([metricGroup, metricGroupData]) => (
+                  <div className="card-body" key={metricType + metricGroup}>
+                    {metricGroup && (
+                      <h5 className="card-title text-center">{metricGroup}</h5>
+                    )}
+
+                    {Object.entries(metricGroupData).map(
+                      ([metric, metricData]) => (
+                        <div
+                          className="row my-2"
+                          key={metricType + metricGroup + metric}
+                        >
+                          <div className="col-md-4">
+                            <p className="card-text text-end">
+                              {metric}{' '}
+                              <sup>
+                                <abbr title={metricData?.tooltip}>?</abbr>
+                              </sup>
+                            </p>
+                          </div>
+                          <div className="col-md-8">
+                            <div
+                              aria-label="Basic radio toggle button group"
+                              className="btn-group btn-group-sm"
+                              role="group"
+                            >
+                              {Object.entries(metricData?.options).map(
+                                ([option, optionsData]) => (
+                                  <button
+                                    className="btn btn-outline-primary"
+                                    title={optionsData?.tooltip}
+                                    type="button"
+                                    key={
+                                      metricType + metricGroup + metric + option
+                                    }
+                                  >
+                                    {option}
+                                  </button>
+                                )
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </div>
+                )
+              )}
+            </div>
+          )
+        )}
+      </div>
+    </div>
   </div>
 );
 
