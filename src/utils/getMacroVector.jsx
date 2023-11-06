cb15c88e27fca342c0b25b44698a68da0303c5d5
Yves Roll <yves.rouille@gmail.com>
2023-11-06 17:51:32 +0100

    getMacroVector

--- src/utils/getMacroVector.jsx
+++ src/utils/getMacroVector.jsx
@@ -6,7 +6,7 @@ export const getMacroVector = (metrics) => {
   // EQ1: 0-AV:N and PR:N and UI:N
   //      1-(AV:N or PR:N or UI:N) and not (AV:N and PR:N and UI:N) and not AV:P
   //      2-AV:P or not(AV:N or PR:N or UI:N)
-  let eq1;
+  let eq1 = 0;
 
   if (get('AV') === 'N' && get('PR') === 'N' && get('UI') === 'N') {
     eq1 = '0';
@@ -25,7 +25,7 @@ export const getMacroVector = (metrics) => {
 
   // EQ2: 0-(AC:L and AT:N)
   //      1-(not(AC:L and AT:N))
-  let eq2;
+  let eq2 = 0;
 
   if (get('AC') === 'L' && get('AT') === 'N') {
     eq2 = '0';
@@ -36,7 +36,7 @@ export const getMacroVector = (metrics) => {
   // EQ3: 0-(VC:H and VI:H)
   //      1-(not(VC:H and VI:H) and (VC:H or VI:H or VA:H))
   //      2-not (VC:H or VI:H or VA:H)
-  let eq3;
+  let eq3 = 0;
 
   if (get('VC') === 'H' && get('VI') === 'H') {
     eq3 = 0;
@@ -52,7 +52,7 @@ export const getMacroVector = (metrics) => {
   // EQ4: 0-(MSI:S or MSA:S)
   //      1-not (MSI:S or MSA:S) and (SC:H or SI:H or SA:H)
   //      2-not (MSI:S or MSA:S) and not (SC:H or SI:H or SA:H)
-  let eq4;
+  let eq4 = 0;
 
   if (get('MSI') === 'S' || get('MSA') === 'S') {
     eq4 = 0;
@@ -71,7 +71,7 @@ export const getMacroVector = (metrics) => {
   // EQ5: 0-E:A
   //      1-E:P
   //      2-E:U
-  let eq5;
+  let eq5 = 0;
 
   if (get('E') === 'A') {
     eq5 = 0;
@@ -83,7 +83,7 @@ export const getMacroVector = (metrics) => {
 
   // EQ6: 0-(CR:H and VC:H) or (IR:H and VI:H) or (AR:H and VA:H)
   //      1-not[(CR:H and VC:H) or (IR:H and VI:H) or (AR:H and VA:H)]
-  let eq6;
+  let eq6 = 0;
 
   if (
     (get('CR') === 'H' && get('VC') === 'H') ||
---
+++ src/utils/getMacroVector.test.jsx
@@ -0,0 +1,90 @@
+import { defaultMetrics } from '../resources/fakeData';
+import { getMacroVector } from './getMacroVector';
+
+describe('getMacroVector', () => {
+  it('should handle macro vector. undefined', () => {
+    expect(getMacroVector(undefined)).toEqual('212201');
+  });
+
+  it('should handle macro vector. default', () => {
+    expect(getMacroVector(defaultMetrics)).toEqual('002201');
+  });
+
+  it('should handle macro vector. EQ1=1', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        AV: 'A',
+      })
+    ).toEqual('102201');
+
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        PR: 'L',
+      })
+    ).toEqual('102201');
+
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        UI: 'P',
+      })
+    ).toEqual('102201');
+  });
+
+  it('should handle macro vector. EQ3=0', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        VC: 'H',
+        VI: 'H',
+      })
+    ).toEqual('000200');
+  });
+
+  it('should handle macro vector. EQ3=1', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        VA: 'H',
+      })
+    ).toEqual('001200');
+  });
+
+  it('should handle macro vector. EQ4=0', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        MSI: 'S',
+      })
+    ).toEqual('002001');
+  });
+
+  it('should handle macro vector. EQ4=1', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        SC: 'H',
+      })
+    ).toEqual('002101');
+  });
+
+  it('should handle macro vector. EQ5=1', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        E: 'P',
+      })
+    ).toEqual('002211');
+  });
+
+  it('should handle macro vector. EQ5=2', () => {
+    expect(
+      getMacroVector({
+        ...defaultMetrics,
+        E: 'U',
+      })
+    ).toEqual('002221');
+  });
+});
